9c7b3a487432d740912c2055dc40241b383c4bb4
nibansal <v-nibansal@expedia.com>
2020-07-20 07:08:30 +0530

    deleting categories and transactions

--- src/main/java/com/expensetrackerapi/expensetracker/repositories/CategoryRepositoryImpl.java
+++ src/main/java/com/expensetrackerapi/expensetracker/repositories/CategoryRepositoryImpl.java
@@ -33,6 +33,11 @@ public class CategoryRepositoryImpl implements CategoryRepository{
     private static final String SQL_UPDATE =  "UPDATE ET_CATEGORIES SET TITLE = ?, DESCRIPTION = ?" +
             " WHERE USER_ID = ? AND CATEGORY_ID = ?";
 
+    private static final String SQL_DELETE_CATEGORY = " DELETE FROM ET_CATEGORIES WHERE USER_ID = ? AND" +
+            " CATEGORY_ID = ?";
+
+    private static final String SQL_DELETE_ALL_TRANSACTIONS = "DELETE FROM ET_TRANSACTIONS WHERE CATEGORY_ID = ?";
+
     @Autowired
     JdbcTemplate jdbcTemplate;
 
@@ -80,7 +85,13 @@ public class CategoryRepositoryImpl implements CategoryRepository{
 
     @Override
     public void removeById(Integer userId, Integer categoryId) throws EtResourceNotFoundException {
+        this.removeAllCatTransactions(categoryId);
+        jdbcTemplate.update(SQL_DELETE_CATEGORY, new Object[]{userId, categoryId});
 
+    }
+
+    private void removeAllCatTransactions(Integer categoryId){
+        jdbcTemplate.update(SQL_DELETE_ALL_TRANSACTIONS, new Object[]{categoryId});
     }
 
     private RowMapper<Category> categoryRowMapper = ((rs, rowNum) -> {
--- src/main/java/com/expensetrackerapi/expensetracker/repositories/TransactionRepositoryImpl.java
+++ src/main/java/com/expensetrackerapi/expensetracker/repositories/TransactionRepositoryImpl.java
@@ -29,6 +29,9 @@ public class TransactionRepositoryImpl implements TransactionRepository{
     private static final String SQL_UPDATE = "UPDATE ET_TRANSACTIONS SET AMOUNT = ?, NOTE = ?, TRANSACTION_DATE = ?" +
             " WHERE USER_ID = ? AND CATEGORY_ID = ? AND TRANSACTION_ID = ?";
 
+    private static final String SQL_DELETE = "DELETE FROM ET_TRANSACTIONS WHERE USER_ID = ? AND CATEGORY_ID = ?" +
+            " AND TRANSACTION_ID = ?";
+
     @Autowired
     JdbcTemplate jdbcTemplate;
 
@@ -82,7 +85,10 @@ public class TransactionRepositoryImpl implements TransactionRepository{
     @Override
     public void removeById(Integer userId, Integer categoryId, Integer transactionId)
             throws EtResourceNotFoundException {
-
+        int count = jdbcTemplate.update(SQL_DELETE, new Object[]{userId, categoryId, transactionId});
+        if(count == 0){
+            throw new EtResourceNotFoundException("Transaction not found");
+        }
     }
 
     private RowMapper<Transaction> transactionRowMapper = ((rs, rowNum) -> {
