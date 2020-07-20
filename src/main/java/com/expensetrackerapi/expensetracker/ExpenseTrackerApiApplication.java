18d9a6d58664cecef6588a471db2c775b8eaa207
nibansal <v-nibansal@expedia.com>
2020-07-20 07:28:32 +0530

    adding cors filter

--- src/main/java/com/expensetrackerapi/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/expensetrackerapi/expensetracker/ExpenseTrackerApiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
@@ -20,5 +23,18 @@ public class ExpenseTrackerApiApplication {
 		registrationBean.setFilter(authFilter);
 		registrationBean.addUrlPatterns("/api/categories/*");
 		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("*"); // allow everyone
+		configuration.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", configuration);
+		filterRegistrationBean.setFilter(new CorsFilter(source));
+		filterRegistrationBean.setOrder(0);
+		return filterRegistrationBean;  
 	}
 }
